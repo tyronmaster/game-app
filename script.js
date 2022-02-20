b96088f8e87869a171fd10fbb96fd18a6cdc803b
tyronmaster <degonski.art@gmail.com>
2022-02-20 21:05:38 +0300

    feat: add cards solving object

--- script.js
+++ script.js
@@ -11,14 +11,17 @@ startButton.forEach( item => item.addEventListener("click", function() {
     body.classList.remove("active");
     gamePlace.classList.add("active");
     ready();
+
+    var sound = new Audio("./assets/sounds/d_shawn3.mp3");
+    sound.play();
+    sound.volume = .75;
+
 }));
-
-
 
 function ready() {
     let cards = Array.from(document.querySelectorAll(".card"));
     let game = new Doom2(100, cards);
-    
+
     game.startGame();
 
     cards.forEach(card => {
@@ -26,4 +29,109 @@ function ready() {
             game.flipCard(card);
         });
     });
+}
+
+
+
+
+/* CRAETE GAME OBJECT */
+
+class Doom2 {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.querySelector(".time__level");
+        this.steps = document.querySelector(".moves__count");
+    }
+
+    startGame() {
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.cardToCheck = null;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffleCards(this.cardsArray);
+            this.countdown = this.startCountdown();
+            this.busy = false;
+        }, 500)
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        this.steps.innerText = this.totalClicks;
+    }
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countdown);
+        document.querySelector(".game__over").classList.add("visible");
+    }
+    victory() {
+        clearInterval(this.countdown);
+        document.querySelector(".win").classList.add("visible");
+    }
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove("visible");
+            card.classList.remove('matched');
+        });
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            //this.sound.flip();
+            this.totalClicks++;
+            this.steps.innerText = this.totalClicks;
+            card.classList.add('visible');
+
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            } else {
+                this.cardToCheck = card;
+            }
+        }
+    }
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else 
+            this.cardMismatch(card, this.cardToCheck);
+
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        //this.sound.match();
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+    shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
+        for (let i = cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
+            cardsArray[randIndex].style.order = i;
+            cardsArray[i].style.order = randIndex;
+        }
+    }
+    getCardType(card) {
+        return card.querySelectorAll(".card__back img")[0].src;
+    }
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
 }
